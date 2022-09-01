826f463878bc32ffd9c9064ffa9791a6fedae4b9
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2022-09-01 15:01:34 -0300

    Programando as classes.

--- App_Hotel/App_Hotel/Model/Hospedagem.cs
+++ App_Hotel/App_Hotel/Model/Hospedagem.cs
@@ -2,13 +2,53 @@
 using System.Collections.Generic;
 using System.Text;
 
+//using System.Transactions;
+
 namespace App_Hotel.Model
 {
 
     public class Hospedagem
     {
 
+        // Variáveis da Classe:
 
+        public Suites quarto { get; set; }
+
+        public int qnt_adultos { get; set; }
+
+        public int qnt_criancas { get; set; }
+
+        public int qnt_dias { get; set; }
+
+        public DateTime data_checkin { get; set; }
+
+        public DateTime data_checkout { get; set; }
+
+        public double valor_total { get; set; }
+
+        // Métodos da Classe:
+
+        public static int Tempo_Estadia(DateTime checkin, DateTime checkout)
+        {
+
+            int total_dias = checkout.Subtract(checkin).Days;
+
+            return total_dias;
+
+        }
+
+        public double Valor_Estadia()
+        {
+
+            double valor_adultos = (qnt_adultos * quarto.valor_diaria_adultos) * qnt_dias;
+
+            double valor_criancas = (qnt_criancas * quarto.valor_diaria_criancas) * qnt_dias;
+
+            double valor_total_hospedagem = valor_adultos + valor_criancas;
+
+            return valor_total_hospedagem;
+
+        }
 
     }
 
---
+++ App_Hotel/App_Hotel/Model/Suites.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App_Hotel.Model
+{
+
+    public class Suites
+    {
+
+        public string descricao { get; set; }
+
+        public double valor_diaria_adultos { get; set; }
+
+        public double valor_diaria_criancas { get; set; }
+
+    }
+
+}
