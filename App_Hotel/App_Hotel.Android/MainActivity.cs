021c7270b3dd63237330d3d0431d74f71867c910
Pablo Valentin <95161515+PabloValentin94@users.noreply.github.com>
2022-10-23 22:13:08 -0300

    Update MainActivity.cs

--- App_Hotel/App_Hotel.Android/MainActivity.cs
+++ App_Hotel/App_Hotel.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace App_Hotel.Droid
 {
-    [Activity(Label = "App_Hotel", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "App Hotel", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
